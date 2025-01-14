4b3eb6cf07ae0452935820d5920ea205fbd776ab
Patrick White <patrick.white@headbox.com>
2025-01-14 16:40:35 +0000

    add test suite for drag and drop updates

--- test/wttj/candidates_test.exs
+++ test/wttj/candidates_test.exs
@@ -39,7 +39,8 @@ defmodule Wttj.CandidatesTest do
       assert {:ok, %Candidate{} = candidate} =
                Candidates.update_candidate(candidate, update_attrs)
 
-      assert candidate.position == 43
+      # As this is the only candidate the position is normalised
+      assert candidate.position == 0
       assert candidate.status == :rejected
       assert candidate.email == email
     end
@@ -53,6 +54,91 @@ defmodule Wttj.CandidatesTest do
     test "change_candidate/1 returns a candidate changeset", %{job1: job1} do
       candidate = candidate_fixture(%{job_id: job1.id})
       assert %Ecto.Changeset{} = Candidates.change_candidate(candidate)
+    end
+
+    test "update_candidate/2 adjusts all candidate positions when decreasing position", %{
+      job1: job1
+    } do
+      candidate_one = candidate_fixture(%{job_id: job1.id, status: :new, position: 0})
+      candidate_two = candidate_fixture(%{job_id: job1.id, status: :new, position: 1})
+      candidate_three = candidate_fixture(%{job_id: job1.id, status: :new, position: 2})
+
+      assert {:ok, updated_candidate} =
+               Candidates.update_candidate(candidate_three, %{position: 0})
+
+      updated_candidates = job1.id |> Candidates.list_candidates() |> Enum.sort_by(& &1.position)
+
+      assert Enum.at(updated_candidates, 0).id == candidate_three.id
+      assert Enum.at(updated_candidates, 1).id == candidate_one.id
+      assert Enum.at(updated_candidates, 2).id == candidate_two.id
+    end
+
+    test "update_candidate/2 adjusts all candidate positions when increasing position", %{
+      job1: job1
+    } do
+      candidate_one = candidate_fixture(%{job_id: job1.id, status: :new, position: 0})
+      candidate_two = candidate_fixture(%{job_id: job1.id, status: :new, position: 1})
+      candidate_three = candidate_fixture(%{job_id: job1.id, status: :new, position: 2})
+
+      assert {:ok, updated_candidate} = Candidates.update_candidate(candidate_one, %{position: 2})
+
+      updated_candidates = job1.id |> Candidates.list_candidates() |> Enum.sort_by(& &1.position)
+
+      assert Enum.at(updated_candidates, 0).id == candidate_two.id
+      assert Enum.at(updated_candidates, 1).id == candidate_three.id
+      assert Enum.at(updated_candidates, 2).id == candidate_one.id
+    end
+
+    test "update_candidate/2 adjusts candidate positions when moving to middle", %{job1: job1} do
+      candidate_one = candidate_fixture(%{job_id: job1.id, status: :new, position: 0})
+      candidate_two = candidate_fixture(%{job_id: job1.id, status: :new, position: 1})
+      candidate_three = candidate_fixture(%{job_id: job1.id, status: :new, position: 2})
+
+      assert {:ok, updated_candidate} = Candidates.update_candidate(candidate_one, %{position: 1})
+
+      updated_candidates = job1.id |> Candidates.list_candidates() |> Enum.sort_by(& &1.position)
+
+      assert Enum.at(updated_candidates, 0).id == candidate_two.id
+      assert Enum.at(updated_candidates, 1).id == candidate_one.id
+      assert Enum.at(updated_candidates, 2).id == candidate_three.id
+    end
+
+    test "update_candidate/2 handles change of position in new status group", %{job1: job1} do
+      candidate_one = candidate_fixture(%{job_id: job1.id, status: :new, position: 0})
+      candidate_two = candidate_fixture(%{job_id: job1.id, status: :interview, position: 0})
+      candidate_three = candidate_fixture(%{job_id: job1.id, status: :interview, position: 1})
+
+      assert {:ok, updated_candidate} =
+               Candidates.update_candidate(candidate_one, %{position: 1, status: :interview})
+
+      updated_candidates = job1.id |> Candidates.list_candidates() |> Enum.sort_by(& &1.position)
+
+      assert updated_candidate.status == :interview
+
+      assert Enum.at(updated_candidates, 0).id == candidate_two.id
+      assert Enum.at(updated_candidates, 1).id == candidate_one.id
+      assert Enum.at(updated_candidates, 2).id == candidate_three.id
+    end
+
+    test "update_candidate/2 enforces valid position bounding", %{job1: job1} do
+      candidate_one = candidate_fixture(%{job_id: job1.id, status: :new, position: 0})
+      candidate_two = candidate_fixture(%{job_id: job1.id, status: :new, position: 1})
+
+      candidate_fixture(%{job_id: job1.id, status: :interview, position: 0})
+      candidate_fixture(%{job_id: job1.id, status: :interview, position: 1})
+
+      candidate_fixture(%{job_id: job1.id, status: :rejected, position: 0})
+      candidate_fixture(%{job_id: job1.id, status: :rejected, position: 1})
+
+      assert {:ok, updated_candidate_one} =
+               Candidates.update_candidate(candidate_one, %{status: :interview, position: -10})
+
+      assert updated_candidate_one.position == 0
+
+      assert {:ok, updated_candidate_two} =
+               Candidates.update_candidate(candidate_two, %{status: :rejected, position: 20})
+
+      assert updated_candidate_two.position == 2
     end
   end
 end
