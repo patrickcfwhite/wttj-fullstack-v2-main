e1e422da8cd24af08b7df3f54ae010cc2c8f031a
Patrick White <patrick.white@headbox.com>
2025-01-14 16:49:43 +0000

    refactored JobShow component and added simple drag initiated styling

--- assets/src/pages/JobShow/index.tsx
+++ assets/src/pages/JobShow/index.tsx
@@ -1,51 +1,22 @@
 import { useParams } from 'react-router-dom'
-import { useJob, useCandidates } from '../../hooks'
-import { Text } from '@welcome-ui/text'
 import { Flex } from '@welcome-ui/flex'
 import { Box } from '@welcome-ui/box'
-import { useMemo } from 'react'
-import { Candidate } from '../../api'
-import CandidateCard from '../../components/Candidate'
-import { Badge } from '@welcome-ui/badge'
-import { DragDropContext, Droppable, DropResult } from '@hello-pangea/dnd'
-
-type Statuses = 'new' | 'interview' | 'hired' | 'rejected'
-const COLUMNS: Statuses[] = ['new', 'interview', 'hired', 'rejected']
-
-interface SortedCandidates {
-  new?: Candidate[]
-  interview?: Candidate[]
-  hired?: Candidate[]
-  rejected?: Candidate[]
-}
+import { Text } from '@welcome-ui/text'
+import { DragDropContext } from '@hello-pangea/dnd'
+import { useJob, useCandidates, useDragHandlers, useSortedCandidates } from '../../hooks'
+import { KanbanColumn } from '../../components/KanbanColumn'
+import { KANBAN_STATUSES } from '../../api'
 
 function JobShow() {
   const { jobId } = useParams()
   const { job } = useJob(jobId)
   const { candidates } = useCandidates(jobId)
-
-  const sortedCandidates = useMemo(() => {
-    if (!candidates) return {}
-
-    return candidates.reduce<SortedCandidates>((acc, c: Candidate) => {
-      acc[c.status] = [...(acc[c.status] || []), c].sort((a, b) => a.position - b.position)
-      return acc
-    }, {})
-  }, [candidates])
-
-  const handleDraggedCandidate = (result: DropResult) => {
-    const { source, destination } = result
-
-    if (!destination) return
-
-    const isSameLocation =
-      source.droppableId === destination.droppableId && source.index === destination.index
-
-    if (!isSameLocation) {
-      // Implement backend changes
-      console.log('update db')
-    }
-  }
+  const sortedCandidates = useSortedCandidates(candidates)
+  const { handleDragUpdate, handleDraggedCandidate } = useDragHandlers({
+    jobId,
+    candidates,
+    sortedCandidates,
+  })
 
   return (
     <>
@@ -55,51 +26,11 @@ function JobShow() {
         </Text>
       </Box>
 
-      <DragDropContext onDragEnd={handleDraggedCandidate}>
+      <DragDropContext onDragUpdate={handleDragUpdate} onDragEnd={handleDraggedCandidate}>
         <Box p={20}>
-          <Flex gap={10}>
-            {COLUMNS.map(column => (
-              <Box
-                key={column}
-                w={300}
-                border={1}
-                backgroundColor="white"
-                borderColor="neutral-30"
-                borderRadius="md"
-                overflow="hidden"
-              >
-                <Flex
-                  p={10}
-                  borderBottom={1}
-                  borderColor="neutral-30"
-                  alignItems="center"
-                  justify="space-between"
-                >
-                  <Text color="black" m={0} textTransform="capitalize">
-                    {column}
-                  </Text>
-                  <Badge>{(sortedCandidates[column] || []).length}</Badge>
-                </Flex>
-                <Droppable key={column} droppableId={column}>
-                  {(
-                    provided
-                    // once functional we can use second provided argument snapshot to update style when dragging
-                  ) => (
-                    <Flex
-                      direction="column"
-                      {...provided.droppableProps}
-                      ref={provided.innerRef}
-                      p={10}
-                      pb={0}
-                    >
-                      {sortedCandidates[column]?.map((candidate, index) => (
-                        <CandidateCard index={index} key={candidate.id} candidate={candidate} />
-                      ))}
-                      {provided.placeholder}
-                    </Flex>
-                  )}
-                </Droppable>
-              </Box>
+          <Flex gap={10} wrap="wrap">
+            {KANBAN_STATUSES.map(column => (
+              <KanbanColumn key={column} column={column} candidates={sortedCandidates[column]} />
             ))}
           </Flex>
         </Box>
