d08a3ab48d1fafc80d6260e0bc923c8e56f6171c
Patrick White <patrick.white@headbox.com>
2025-01-15 21:03:06 +0000

    implement useEffect to watch for updates to candidates

--- assets/src/pages/JobShow/index.tsx
+++ assets/src/pages/JobShow/index.tsx
@@ -3,20 +3,41 @@ import { Flex } from '@welcome-ui/flex'
 import { Box } from '@welcome-ui/box'
 import { Text } from '@welcome-ui/text'
 import { DragDropContext } from '@hello-pangea/dnd'
-import { useJob, useCandidates, useDragHandlers, useSortedCandidates } from '../../hooks'
+import {
+  useJob,
+  useCandidates,
+  useDragHandlers,
+  useSortedCandidates,
+  useChannel,
+} from '../../hooks'
 import { KanbanColumn } from '../../components/KanbanColumn'
 import { KANBAN_STATUSES } from '../../api'
+import { useEffect } from 'react'
+import { useQueryClient } from 'react-query'
 
 function JobShow() {
   const { jobId } = useParams()
   const { job } = useJob(jobId)
   const { candidates } = useCandidates(jobId)
   const sortedCandidates = useSortedCandidates(candidates)
+  const queryClient = useQueryClient()
+  const [jobCandidatesChannel] = useChannel(`candidate:${jobId}`)
   const { handleDragUpdate, handleDraggedCandidate } = useDragHandlers({
     jobId,
     candidates,
     sortedCandidates,
   })
+
+  useEffect(() => {
+    if (!jobCandidatesChannel || !queryClient || !jobId) return
+
+    jobCandidatesChannel.on('candidate_updated', _updatedCandidate => {
+      queryClient.invalidateQueries(['candidates', jobId])
+    })
+    return () => {
+      jobCandidatesChannel.off('candidate_updated')
+    }
+  }, [jobCandidatesChannel, candidates, jobId, queryClient])
 
   return (
     <>
